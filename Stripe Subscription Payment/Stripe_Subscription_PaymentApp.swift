cae2d6df30b1e859cde387062749e6f24f225e18
vikas kumar <vkbookworm@gmail.com>
2021-10-24 12:43:03 +0530

    feat: added the initial code

---
+++ Stripe Subscription Payment/Stripe_Subscription_PaymentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stripe_Subscription_PaymentApp.swift
+//  Stripe Subscription Payment
+//
+//  Created by Vikas on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Stripe_Subscription_PaymentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
